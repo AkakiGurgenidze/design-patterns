df68944270e2e2a4b3e56fb053739753e27877a3
GuRgEnA <agurg21@freeuni.edu.ge>
2023-12-29 19:02:11 +0400

    Make library runnable

--- library/runner/cli.py
+++ library/runner/cli.py
@@ -10,7 +10,7 @@ cli = Typer(no_args_is_help=True, add_completion=False)
 
 
 @cli.command()
-def run(host: str = "0.0.0.0", port: int = 8000) -> None:
+def run(host: str = "127.0.0.1", port: int = 8000) -> None:
     load_dotenv()
 
     uvicorn.run(host=host, port=port, app=init_app())
